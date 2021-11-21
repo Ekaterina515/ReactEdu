d8b2c2b004818176bf79e9fdb04bf95956064d3c
EkaterinaF <9150221@mail.ru>
2021-11-21 19:21:22 +0200

    Lesson 2 with problem

--- src/index.js
+++ src/index.js
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { useState } from "react";
 import "./index.css";
 
-function Message(props) {
-  return (
-    <h1>
-      Project: <span class="title"> {props.title} </span>
-    </h1>
-  );
-}
+//
 
-function App() {
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState("");
+
+  // eslint-disable-next-line no-undef
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const lastMessages = messages[messages.lenght - 1];
+    let timerId = null;
+
+    if (messages.lenght && lastMessages.author !== "Bot") {
+      timerId = setTimeout(() => {
+        setMessages([
+          ...messages,
+          { author: "Bot", message: "Hello! How can I help you?" },
+        ]);
+      }, 500);
+    }
+
+    return () => clearInterval(timerId);
+  }, [messages]);
+
+  useEffect(() => {
+    ref.current?.focus();
+  }, []);
+
   return (
     <div>
-      <Message title="React Chat" />
+      {messages.map((message) => (
+        <div>{message.message}</div>
+      ))}
+
+      <input
+        ref={ref}
+        placeholder="enter message..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          setMessages([...messages, { author: "User", message: value }]);
+          setValue("");
+        }}
+      >
+        send message
+      </button>
     </div>
   );
-}
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
